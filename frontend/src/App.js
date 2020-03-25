0818994df9fb6e7ca2fcf816847de0c77912861e
Ygor-sLima <ygor.lima@bandtec.com.br>
2020-03-24 23:23:26 -0300

    Backend da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Omnistack!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Ygor-sLima"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          By Ygor
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
